ade8a9d52610bc9ca9b900b87276dec9d2cdeeaf
Kamil <107948849+K4maS@users.noreply.github.com>
2025-03-28 01:29:58 +0500

    Изменен readme

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# git push -f git@github.com:K4maS/posts-test-mitrasoft.git master:gh-pages
+git push -f git@github.com:K4maS/WorkTrack.git master:gh-pages
 
 cd -
